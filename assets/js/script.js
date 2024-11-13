420318cf4379d27781ad47f1f49e7d21a4066a4f
Marlon <iammarlonayala@gmail.com>
2024-11-13 01:37:25 -0500

    actualización de experiencia y otros

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 
 
 
-// element toggle function
+// alternar elementos
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
 
@@ -16,7 +16,7 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
+// lenguajes  variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
@@ -27,13 +27,13 @@ const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
+// modal funcion toggle
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
+// agregar evento de clic a todos los elementos modales
 for (let i = 0; i < testimonialsItem.length; i++) {
 
   testimonialsItem[i].addEventListener("click", function () {
@@ -49,13 +49,13 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
+// agregar evento de clic al botón de cierre modal
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
 
 
-// custom select variables
+// variables de selección personalizadas
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
@@ -63,7 +63,7 @@ const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// add event in all select items
+// agregar evento en todos los elementos seleccionados
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
 
@@ -75,7 +75,7 @@ for (let i = 0; i < selectItems.length; i++) {
   });
 }
 
-// filter variables
+// variable de filters 
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -94,7 +94,7 @@ const filterFunc = function (selectedValue) {
 
 }
 
-// add event in all filter button items for large screen
+// agregar evento en todos los elementos del botón de filtro para pantalla grande
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -115,12 +115,12 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 
 
-// contact form variables
+// variables del formulario de contacto
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// agregar evento a todos los campos de entrada del formulario
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
 
@@ -136,7 +136,7 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// page navigation variables
+// variables de navegación de página
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
